cd00456173d063c4fc3a965164a50cbce9edbdf2
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2021-03-24 15:44:25 +0100

    update decidim subcommand detail

--- lib/bruw/commands/decidim.rb
+++ lib/bruw/commands/decidim.rb
@@ -4,6 +4,9 @@ module Bruw
   module Commands
     class Decidim < Thor
       desc 'version', 'Get the current decidim version'
+      long_desc <<-LONGDESC
+        Checks the Decidim version for the current project based on the bundler gem infos
+      LONGDESC
       def version
         puts "Current Decidim version is #{Bruw::Decidim.version&.colorize(:green)}"
       rescue StandardError => e
