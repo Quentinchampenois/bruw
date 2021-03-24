3bc3e57c0fc29a0208320107150632f7733a08e5
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2021-03-24 16:40:31 +0100

    implement curl command

--- lib/bruw/commands/decidim.rb
+++ lib/bruw/commands/decidim.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'thor'
+require "net/http"
 
 module Bruw
   module Commands
@@ -11,6 +12,24 @@ module Bruw
       LONGDESC
       def version
         puts "Current Decidim version is #{Bruw::Decidim.version&.colorize(:green)}"
+      rescue StandardError => e
+        puts e.message.colorize(:red)
+      end
+
+      desc "curl [RELATIVE_PATH]", "Curl given file against target github repository"
+      long_desc <<-LONGDESC
+        Allows to curl source code of specified path for a given version
+      LONGDESC
+      option :tag, required: false, banner: "Tag must have format vx.xx.x : Default current decidim version",
+                   aliases: "-t", type: :string
+      option :owner, required: false, banner: "Owner name : Default 'decidim'", aliases: "-o", type: :string
+      option :repo, required: false, banner: "Repository name : Default 'decidim'", aliases: "-r", type: :string
+      option :save, required: false, banner: "Save output in relative path", aliases: "-s", type: :boolean
+      def curl(path)
+        options[:path] = path
+        options[:version] = options[:tag]
+
+        puts Bruw::Decidim.curl(options)
       rescue StandardError => e
         puts e.message.colorize(:red)
       end
--- spec/decidim_spec.rb
+++ spec/decidim_spec.rb
@@ -21,19 +21,19 @@ RSpec.describe Bruw::Decidim do
     end
   end
 
-  describe '#curl' do
+  describe "#curl" do
     it "curl the target file on Decidim's official repository" do
       expect(false).to be_truthy
     end
 
-    context 'when selecting a specific version' do
+    context "when selecting a specific version" do
       it "curl the target file from the specified version on Decidim's official repository" do
         expect(false).to be_truthy
       end
     end
 
     context "when file does not exists on Decidim's official repository" do
-      it 'raises an error' do
+      it "raises an error" do
         expect(false).to be_truthy
       end
     end
