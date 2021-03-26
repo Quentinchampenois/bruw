c3ba3df7b0e63d1892f25562b1e74d8faabd01b7
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2021-03-26 10:18:11 +0100

    add compression and decompression commands

--- lib/bruw/base.rb
+++ lib/bruw/base.rb
@@ -5,5 +5,18 @@ module Bruw
     def self.version
       "0.0.1"
     end
+
+    def self.strip_path(path)
+      return if path.nil? || path.empty?
+      return path[0..-2] if path.end_with? "/"
+
+      path
+    end
+
+    def self.create_path(path)
+    return if Dir.exist?(path)
+
+    FileUtils.mkpath(path)
+    end
   end
 end
--- lib/bruw/cli.rb
+++ lib/bruw/cli.rb
@@ -9,6 +9,34 @@ module Bruw
       puts VERSION
     end
 
+    desc "zip TARGET DESTINATION", "Compress target"
+    def zip(target, destination = ".")
+      target = Bruw::Base.strip_path(target)
+      dest = Bruw::Base.strip_path(destination)
+      final = "#{dest}/#{target}.zip"
+
+      raise StandardError, "File already existing : #{final}" if File.exist?(final)
+
+      Bruw::Base.create_path(dest) unless Dir.exist?(dest)
+
+      `zip -r #{final} #{target}`
+      puts "Compression finished, find file at".colorize(:green)
+      puts "> #{final}"
+    rescue StandardError => e
+      puts e.message.colorize(:red)
+    end
+
+    desc "unzip TARGET", "Compress target"
+    def unzip(target)
+      raise StandardError, "File '#{target}' does not exists" unless File.exist?(target)
+
+      `unzip #{target}`
+      puts "Decompression finished, find file at".colorize(:green)
+      puts "> #{target}"
+    rescue StandardError => e
+      puts e.message.colorize(:red)
+    end
+
     desc "decidim SUBCOMMAND ...ARGS", "Manage Decidim projects"
     subcommand "decidim", Bruw::Commands::Decidim
 
